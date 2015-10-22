45f3b6c67cefe25a913ac4161c6a6461e65cabd3
ramalamadingdong <ramimouro@gmail.com>
2015-10-22 06:35:50 -0400

    10.22.15 -> 12:11
    with Mr. Hoyt's Code Full
    
    Edited

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -61,7 +61,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("NullOp", NullOp.class);
     // manager.register("AluminatiAuto",AluminatiAuto.class);
     // manager.register("AluminatiTeleOp",AluminatiTeleop.class);
-    manager.register("AluminatiImu", imuDemo.class);
+    manager.register("imuDemo", imuDemo.class);
     // manager.register("IMUTest",IMUtest.class);
   }
 }
